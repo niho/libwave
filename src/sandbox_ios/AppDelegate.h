8d856af040bef4925b050597f48f63de8393b084
per <per@stuffmatic.com>
2014-09-01 11:05:41 +0200

    pulled in kowalski as referenced xcode project, set up header search paths and lib linking

---
+++ src/sandbox_ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DiggerRecorder
+//
+//  Created by perarne on 9/1/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ src/sandbox_ios/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DiggerRecorder
+//
+//  Created by perarne on 9/1/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
