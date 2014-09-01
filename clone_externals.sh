4e28d80ea4d07d21a755895c8b85a9fa516cac5a
per <per@stuffmatic.com>
2014-09-01 10:47:47 +0200

    added empty xcode project and a script to clone external dependencies

---
+++ clone_externals.sh
@@ -0,0 +1,10 @@
+#create external dir if needed
+
+if [ ! -d "external" ]; then
+    echo 'creating external dir'
+    mkdir external
+fi
+
+cd external
+git clone https://github.com/stuffmatic/kowalski.git
+cd ..
